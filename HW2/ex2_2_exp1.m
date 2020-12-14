666f82bdb9ae6596047e070abf0f5c72c96ad06e
Alex Fok <alexfokil@gmail.com>
2020-12-14 17:47:02 +0200

    Clean version

--- HW2/ex2_2_exp1.m
+++ HW2/ex2_2_exp1.m
@@ -1,14 +1,11 @@
-repeates = 10;
 % Load data file
 cifar_data = load('EX2q2_data.mat');
 
 n = [0:15];
-%n = [3,6,9,12,15];
 lambdas = zeros(length(n), 1);
 
 % Experiment 1
 meantestErrors = zeros(length(n), 1);
-%minMaxErrors = zeros(length(n), 2);
 minMaxTestErrors = zeros(length(n), 2);
 
 meantrainErrors = zeros(length(n), 1);
@@ -18,8 +15,6 @@ Xtest = cifar_data.Xtest;
 Ytest = cifar_data.Ytest;
 Xtrain = cifar_data.Xtrain;
 Ytrain = cifar_data.Ytrain;
-% [Xtest, Ytest] = gensmall_sample(cifar_data.Xtest, cifar_data.Ytest, 500)
-% [Xtrain, Ytrain] = gensmall_sample(cifar_data.Xtrain, cifar_data.Ytrain, 500)
 
 
 for i=1:length(n)
@@ -32,39 +27,23 @@ for i=1:length(n)
     fprintf("Finished calculating softsvm, iteration: %d, time: %f\n", i, t);
     % Calculate iteration error
     
-%    testIterErrors = sign(Xtest*w) ~= Ytest;
     meantestErrors(i) = mean(sign(Xtest*w) ~= Ytest);
     disp("meantestErrors(i):");
     disp(meantestErrors(i));
-%    minMaxTestErrors(i,:) = [min(testIterErrors), max(testIterErrors)];
-    
-%    trainIterErrors = sign(Xtrain*w) ~= Ytrain;
     meantrainErrors(i) = mean(sign(Xtrain*w) ~= Ytrain);
     disp("meantrainErrors(i):");
     disp(meantrainErrors(i));
-%    minMaxTrainErrors(i,:) = [min(trainIterErrors), max(trainIterErrors)];
-    
+   
 end
 %meanErrors = mean(errors);
 %minMaxErrors = [min(meanErrors), max(meanErrors)];
 
-% TODO:
 % Calculate lambdas and plot it on logarithmic scale
 disp("Avg: ")
 disp(meantestErrors);
-disp("Min Max: ")
-disp(minMaxTestErrors);
 
 disp("Avg: ")
 disp(meantrainErrors);
-disp("Min Max: ")
-disp(minMaxTrainErrors);
-
-% figure
-% loglog(lambdas, meantestErrors, lambdas, meantrainErrors)
-% xlabel("Lambda");
-% ylabel("Average sample error");
-% legend('Test Error','Train Error','Location','southeast')
 
 figure
 a1 = plot(n, meantestErrors);
@@ -76,14 +55,3 @@ M1 = "Test Error";
 M2 = "Train Error";
 legend([a1;a2], M1, M2, 'Location','southeast')
 hold('off');
-
-% figure
-% a1 = bar(n, minMaxTestErrors);
-% hold('on');
-% a2 = bar(n, minMaxTrainErrors);
-% xlabel("Lambda");
-% ylabel("Sampel size minMaxErrors");
-% M1 = "minMaxTestErrors";
-% M2 = "minMaxTrainErrors";
-% %legend([a1;a2], M1, M2)
-% hold('off');
--- HW2/ex2_2_exp2.m
+++ HW2/ex2_2_exp2.m
@@ -1,43 +1,55 @@
+% Experiment 2
 repeates = 10;
 % Load data file
 cifar_data = load('EX2q2_data.mat');
 
-n = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
+n = [0:15];
+%n = [3,6,9,12,15];
+lambdas = zeros(length(n), 1);
 
-meanErrors = zeros(length(n), 1);
-minMaxErrors = zeros(length(n), 2);
-minErrors = zeros(length(n), 1);
+iterTrainErrors = zeros(repeates, 1);
+iterTestErrors = zeros(repeates, 1);
 
-errors = zeros(repeates, 1);
+meanTrainErrors = zeros(length(n), 1);
+meanTestErrors = zeros(length(n), 1);
+minMaxTrainErrors = zeros(length(n), 2);
+
 for i=1:length(n)
-    % lambda - algorithm parameter
-    lambda = 10^n(i);
-    % Generate test w for validation
-    [smallTestX, smallTestY] = gensmall_sample(cifar_data.Xtest, cifar_data.Ytest, 100)
-    WTest = softsvm(lambda, smallTestX, smallTestY);
-    for j=1:repeates
-        % draw sample size 100
-        [smallX, smallY] = gensmall_sample(cifar_data.Xtrain, cifar_data.Ytrain, 100)
-        WTrain = softsvm(lambda, smallX, smallY);
-        errors(j) = mean(WTest ~= WTrain);
+    for j=repeates
+        % lambda - algorithm parameter
+        lambdas(i) = 10^n(i);
+        fprintf("Calculating softsvm, iteration: %d\n", i);
+        [Xtrain, Ytrain] = gensmall_sample(cifar_data.Xtrain, cifar_data.Ytrain, 100);
+        [Xtest, Ytest] = gensmall_sample(cifar_data.Xtest, cifar_data.Ytest, 100);
+        tic;
+        w = softsvm(lambdas(i), Xtrain, Ytrain);
+        t = toc;
+        fprintf("Finished calculating softsvm, iteration: %d, time: %f\n", i, t);
+        % Calculate iteration error
+        iterTrainErrors(j) = mean(sign(Xtrain*w) ~= Ytrain);
+        iterTestErrors(j) = mean(sign(Xtest*w) ~= Ytest);
     end
-    meanErrors(i) = mean(errors);
-    minMaxErrors(i,:) = [min(errors), max(errors)];
+    meanTrainErrors(i) = mean(iterTrainErrors);
+    meanTestErrors(i) = mean(iterTestErrors);
+    minMaxTrainErrors(i,:) = [min(iterTrainErrors), max(iterTrainErrors)];
 end
-%meanErrors = mean(errors);
-%minMaxErrors = [min(errors), max(errors)];
 
-disp("Avg: ")
-disp(meanErrors);
-disp("Min Max: ")
-disp(minMaxErrors);
-
-figure
-plot(n, meanErrors);
-xlabel("Lambda");
-ylabel("Average sample size error");
+% Calculate lambdas and plot it on logarithmic scale
+% disp("minTrainErrors: ")
+% disp(minTrainErrors);
+disp("meantrainErrors: ")
+disp(meantrainErrors);
 
 figure
-bar(n, minMaxErrors);
-xlabel("Lambda");
-ylabel("Sampel size error");
+plot(n, meanTrainErrors);
+xlabel("log(Lambda)");
+ylabel("Average sample error");
+hold('on');
+plot(n, meanTestErrors);
+legend('train error', 'test error', 'Location','southeast')
+hold('off');
+
+figure
+bar(n, minMaxTrainErrors);
+xlabel("log(Lambda)");
+ylabel("min Max Train Errors");
